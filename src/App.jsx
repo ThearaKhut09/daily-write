9f3b6f3b87a28f00781060d0a69bccf79526fad4
Ratanak123456 <ratanaksare3@gmail.com>
2026-02-24 14:46:26 +0700

    fix style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import './App.css'
-import HeroSection from './components/homepage/HeroSection'
-import NavbarComponent from './components/Navbar/NavbarComponent'
-import { Outlet } from 'react-router-dom'
+import "./App.css";
+import ImageSide from "./components/homepage/HeroSection/ImageSide";
+import Information from "./components/homepage/HeroSection/Information";
 
 function App() {
-
   return (
     <main>
-      <HeroSection/>
+      {/* Hero Section */}
+      <section className="bg-[#FFF8ED] dark:bg-black relative overflow-hidden py-12 lg:py-0 ">
+        <div className="absolute -top-40 -left-40 md:-top-80 md:-left-80 w-100 h-100 md:w-200 md:h-200 rounded-full opacity-40 pointer-events-none animate-spin-slow bg-[radial-gradient(circle_at_center,#F48024_0%,transparent_70%)]" />
+
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <div className="absolute top-10 right-10 md:top-20 md:right-20 w-2 h-2 md:w-4 md:h-4 bg-primary-orange/20 rounded-full animate-float-slow" />
+          <div className="absolute bottom-20 left-10 md:bottom-40 md:left-20 w-3 h-3 md:w-6 md:h-6 bg-primary-orange/10 rounded-full animate-float-delayed" />
+          <div className="absolute top-20 left-1/4 md:top-40 w-2 h-2 md:w-3 md:h-3 bg-primary-orange/15 rounded-full animate-pulse-slow" />
+        </div>
+
+        <div className="container mx-auto px-4 sm:px-6 lg:px-20 grid lg:grid-cols-2 gap-8 md:gap-12 lg:gap-16 items-center relative z-10">
+          <Information />
+          <ImageSide />
+        </div>
+        <div className="absolute -bottom-100 -right-50 w-100 h-100 md:w-200 md:h-200 rounded-full opacity-40 pointer-events-none animate-spin-slow bg-[radial-gradient(circle_at_center,#F48024_0%,transparent_70%)]" />
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Button({title, backgroundColor, hoverColor, textColor = "text-white"}) {
+export default function Button({
+  link,
+  title,
+  backgroundColor,
+  hoverColor,
+  textColor = "text-white",
+}) {
   return (
-    <button className={`px-8 py-3 ${backgroundColor} ${textColor} font-bold rounded-lg hover:${hoverColor} transition-all shadow-lg shadow-orange-200 dark:shadow-orange-900/30`}>
-      {title}
-    </button>
+    <Link to={`/${link}`}>
+      <button
+        className={`px-8 py-3 ${backgroundColor} ${textColor} font-bold rounded-lg hover:${hoverColor} transition-all shadow-lg shadow-orange-200 dark:shadow-orange-900/30`}
+      >
+        {title}
+      </button>
+    </Link>
   );
 }
